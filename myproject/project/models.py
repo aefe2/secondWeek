41963d39176a8574467fa3dc5ffc2e950ea3a530
Kakayou <shigaev_1990@mail.ru>
2022-11-14 01:21:11 +0700

    Created model User

--- myproject/project/models.py
+++ myproject/project/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+# сущности - пользователь, заявка, категория
+
+class User(AbstractUser):
+    fname = models.CharField(max_length=150, verbose_name='Имя', blank=False)
+    lname = models.CharField(max_length=150, verbose_name='Фамилия', blank=False)
+    sname = models.CharField(max_length=150, verbose_name='Отчество', blank=True)
+    username = models.CharField(max_length=150, verbose_name='Логин', unique=True, blank=False)
+    email = models.CharField(max_length=250, verbose_name='Почта', unique=True, blank=False)
+    password = models.CharField(max_length=250, verbose_name='Пароль', blank=False)
+    data_processing = models.BooleanField(verbose_name='Согласие на обработку персональных данных', blank=False)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.fname

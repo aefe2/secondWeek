9b6354eb15f2bb613ac4f374a1dc62a46cded3e3
Kakayou <shigaev_1990@mail.ru>
2022-11-14 01:57:02 +0700

    Created models Apl-n, category, validations to them

--- myproject/project/models.py
+++ myproject/project/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 # Create your models here.
@@ -18,3 +21,33 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.fname
+
+
+def get_name_file(instance, filename):
+    return '/'.join([get_random_string(length=5) + '_' + filename])
+
+
+def file_size(value):
+    limit = 2 * 1024 * 1024
+    if value.size > limit:
+        raise ValidationError('Файл слишком большой, он не должен весить больше 2Mb')
+
+
+class Aplication(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Название', blank=False)
+    description = models.CharField(max_length=250, verbose_name='Описание', blank=False)
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.CASCADE)
+    photo = models.ImageField(max_length=250, upload_to=get_name_file,
+                              blank=False, null=True,
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'bmp']),
+                                          file_size])
+
+    def __str__(self):
+        return self.name
+
+
+class Category:
+    name = models.CharField(max_length=250, verbose_name='Название', blank=False)
+
+    def __str__(self):
+        return self.name

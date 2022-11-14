1672d91a52b3b9996f0cdc0049623887268be87c
Kakayou <shigaev_1990@mail.ru>
2022-11-15 01:38:27 +0700

    register view

--- myproject/project/views.py
+++ myproject/project/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
 
-def login(request):
-    return render(request, 'registration/basic.html')
+class RegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('login')
